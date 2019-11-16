6197de1391160cd47fcfff8314ef99797efbb779
github username <jonathansychan@gmail.com>
2019-11-16 18:23:42 -0500

    Adds drawing mechanic

--- basic-shapes/canvas.js
+++ basic-shapes/canvas.js
@@ -4,24 +4,44 @@ window.addEventListener("load", () => {
     const ctx = canvas.getContext('2d');
     let painting = false;
 
-    function startPosition(){
-        painting = true
+    function startPosition(e){
+        painting = true;
+        draw(e);
     }
     function finishedPosition(){
         painting = false;
+        ctx.beginPath()
     }
+    function draw(e) {
+        if(!painting) return 
+        ctx.lineWidth = 2;
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = "purple"
+
+        ctx.lineTo(e.clientX, e.clientY);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(e.clientX, e.clientY);
+    }
+
+    canvas.addEventListener("mousedown", startPosition);
+    canvas.addEventListener("mouseup", finishedPosition);
+    canvas.addEventListener("mousemove", draw);
     // Eventlisteners 
     
     // // Resize
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
 
-    // Make a shape - works like position absolute
-    // x coordinate, y y coordinate, height, width
+    // ctx.fillRect => Makes a shape - works like position absolute
+    // x coordinate, y y coordinate, width, height
     ctx.fillRect(100, 100, 200, 500);
+    // Change stroke colors
     ctx.strokeStyle = "red";
+    // Changes line width
     ctx.lineWidth = 5;
     ctx.strokeRect(500, 100, 200, 200);
+    // Works top to down.
     ctx.strokeStyle = "green";
     ctx.lineWidth = 2;
     ctx.strokeRect(450, 150, 200, 200);
@@ -33,6 +53,9 @@ window.addEventListener("load", () => {
     ctx.lineTo(550,600);
     ctx.strokeStyle = "blue"
     ctx.stroke();
+
+    // Reset path
+    ctx.beginPath()
 
     window.addEventListener("resize", () => {
         canvas.height = window.innerHeight;
